3ae010eeb621bd7113d47d0dc3eaa43449ebde27
JoshuaPavan <joshuapavan35@gmail.com>
2024-01-20 23:54:10 +0530

    Update BloggersApplicationTests.java

--- src/test/java/com/bloggersApp/bloggers/BloggersApplicationTests.java
+++ src/test/java/com/bloggersApp/bloggers/BloggersApplicationTests.java
@@ -1,13 +1,13 @@
-package com.bloggersApp.bloggers;
+// package com.bloggersApp.bloggers;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+// import org.junit.jupiter.api.Test;
+// import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
-class BloggersApplicationTests {
+// @SpringBootTest
+// class BloggersApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+// 	@Test
+// 	void contextLoads() {
+// 	}
 
-}
+// }
